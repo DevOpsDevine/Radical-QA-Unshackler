0bd99b3bedebaa3c81f80265b37251e4d9f794d7
Mike Devine <y-mdevine@daptiv.com>
2012-04-17 16:42:16 -0700

    first running tests (google)

---
+++ src/RadicalQA/Radical.Tests/GoogleTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium.Remote;
+using log4net;
+
+namespace Radical.Tests
+{
+    public class GoogleTests:SpecificationForAllBrowsers
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(WebDriverFactoryTest));
+
+        public override void SpecifyForBrowser(RemoteWebDriver driver)
+        {
+            given("a non null driver", delegate()
+            {
+                Assert.IsNotNull(driver);
+                when("we goto google.com", delegate()
+                {                                                                                   
+                    driver.Navigate().GoToUrl("http://google.com");
+                    then("we can get the user agent",
+                        delegate()
+                            {
+                                expect(() => GetUserAgent(driver).Length >0);
+                            });
+                });
+
+            });
+        }
+
+        private string GetUserAgent(RemoteWebDriver driver)
+        {
+            return driver.ExecuteScript("return window.navigator.userAgent;").ToString();
+        }
+    }
+}
---
+++ src/RadicalQA/Radical.Tests/SpecificationForAllBrowsers.cs
@@ -0,0 +1,45 @@
+﻿using NJasmine;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using Radical.Infrastructure;
+using log4net;
+
+namespace Radical.Tests
+{
+    public abstract class SpecificationForAllBrowsers : GivenWhenThenFixture
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(WebDriverFactoryTest));
+        string[] SupportedBrowsers = new string[] { BrowserNames.Chrome, BrowserNames.Firefox, BrowserNames.InternetExplorer };
+
+
+        public abstract void SpecifyForBrowser(RemoteWebDriver browser);
+
+        public sealed override void Specify()
+        {
+            foreach(string browserName in SupportedBrowsers)
+            {
+                given(browserName, delegate
+                {
+                    SpecifyForBrowser(CreateWebDriver(browserName));  
+                });
+            }
+        }
+
+        private static WebStartupSettings LoadSettings()
+        {
+            return new WebStartupSettings();
+        }
+
+        private RemoteWebDriver CreateWebDriver(string browserType)
+        {
+            var settings = LoadSettings();
+            settings.RunTestsUsingSeleniumGrid = false;
+            settings.BrowserType = browserType;
+
+            return new WebDriverFactory(settings).CreateWebDriver();
+            
+        }
+
+    }
+}
